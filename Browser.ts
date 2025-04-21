31591f7507878e814f2f44a70164f1f8051e55bd
Light <123307773+LightZirconite@users.noreply.github.com>
2025-04-21 19:24:08 +0200

    Add files via upload

---
+++ Browser.ts
@@ -0,0 +1,86 @@
+import playwright, { BrowserContext } from 'rebrowser-playwright'
+
+import { newInjectedContext } from 'fingerprint-injector'
+import { FingerprintGenerator } from 'fingerprint-generator'
+
+import { MicrosoftRewardsBot } from '../index'
+import { loadSessionData, saveFingerprintData } from '../util/Load'
+import { updateFingerprintUserAgent } from '../util/UserAgent'
+
+import { AccountProxy } from '../interface/Account'
+
+/* Test Stuff
+https://abrahamjuliot.github.io/creepjs/
+https://botcheck.luminati.io/
+https://fv.pro/
+https://pixelscan.net/
+https://www.browserscan.net/
+*/
+
+class Browser {
+    private bot: MicrosoftRewardsBot
+
+    constructor(bot: MicrosoftRewardsBot) {
+        this.bot = bot
+    }
+
+    async createBrowser(proxy: AccountProxy, email: string): Promise<BrowserContext> {
+        const browser = await playwright.chromium.launch({
+            //channel: 'msedge', // Uses Edge instead of chrome
+            headless: this.bot.config.headless,
+            ...(proxy.url && { proxy: { username: proxy.username, password: proxy.password, server: `${proxy.url}:${proxy.port}` } }),
+            args: [
+                '--no-sandbox',
+                '--mute-audio',
+                '--disable-setuid-sandbox',
+                '--ignore-certificate-errors',
+                '--ignore-certificate-errors-spki-list',
+                '--ignore-ssl-errors',
+                '--disable-features=IsolateOrigins,site-per-process' // Helps resolve issues with the new authentication interface
+            ]
+        })
+
+        const sessionData = await loadSessionData(this.bot.config.sessionPath, email, this.bot.isMobile, this.bot.config.saveFingerprint)
+
+        const fingerprint = sessionData.fingerprint ? sessionData.fingerprint : await this.generateFingerprint()
+
+        const context = await newInjectedContext(browser as any, { 
+            fingerprint: fingerprint,
+            newContextOptions: {
+                viewport: this.bot.isMobile ? { width: 390, height: 844 } : { width: 1280, height: 720 }
+            }
+        })
+
+        // Set timeout to preferred amount with additional wait time for the new interface
+        context.setDefaultTimeout(this.bot.utils.stringToMs(this.bot.config?.globalTimeout ?? 45_000))
+
+        // Allow popups for the new authentication interface
+        await context.setExtraHTTPHeaders({
+            'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7'
+        })
+        
+        await context.addCookies(sessionData.cookies)
+
+        if (this.bot.config.saveFingerprint) {
+            await saveFingerprintData(this.bot.config.sessionPath, email, this.bot.isMobile, fingerprint)
+        }
+
+        this.bot.log(this.bot.isMobile, 'BROWSER', `Created browser with User-Agent: "${fingerprint.fingerprint.navigator.userAgent}"`)
+
+        return context as BrowserContext
+    }
+
+    async generateFingerprint() {
+        const fingerPrintData = new FingerprintGenerator().getFingerprint({
+            devices: this.bot.isMobile ? ['mobile'] : ['desktop'],
+            operatingSystems: this.bot.isMobile ? ['android'] : ['windows'],
+            browsers: [{ name: 'edge' }]
+        })
+
+        const updatedFingerPrintData = await updateFingerprintUserAgent(fingerPrintData, this.bot.isMobile)
+
+        return updatedFingerPrintData
+    }
+}
+
+export default Browser
